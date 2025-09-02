d306b92640a0a01cd343db1a5af71953dd800266
RickySolon-UCM <Solonr491@gmail.com>
2025-09-02 17:08:27 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("FOREVER YOUNG [Alphaville]");
+        System.out.println("Forever young, I want to be forever young");
+        System.out.println("Do you really want to live forever?");
+        System.out.println("Forever, and ever");
+        System.out.println("Forever young, I want to be forever young");
+        System.out.println("Do you really want to live forever?");
+        System.out.println("Forever young");
+    }
+}
