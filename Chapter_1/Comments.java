3bc0a5c674831a5a0caf792d72abce100a8cb3f0
RickySolon-UCM <Solonr491@gmail.com>
2025-09-02 17:32:37 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * This is the Comments class.
+ * It demonstrates different types of comments in Java.
+ */
+public class Comments {
+    
+    public static void main(String[] args) {
+        
+        // Single-line Comment: Prints a message to the console
+        System.out.println("Hello, World!"); 
+        
+        /*
+         * Multi-line Comment:
+         * The next line will display a simple message.
+         * It shows how multi-line comments work.
+         */
+        System.out.println("This is a multi-line comment example.");
+    }
+}
