9bbb78d2af35ebffdf1ff73004c3c84c32f1e5e7
RickySolon-UCM <Solonr491@gmail.com>
2025-09-15 22:31:40 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,13 @@
+public class CompanyInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Ricky Electronics");
+        DisplayHours();
+    }
+
+    public static void DisplayHours() {
+        System.out.println("Monday - Friday   8 am to 6 pm");
+        System.out.println("Saturday          8 am to noon");
+        System.out.println("Sunday            closed");
+    }
+}
