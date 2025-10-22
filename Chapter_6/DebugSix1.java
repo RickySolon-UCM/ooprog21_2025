2e19e3a888397020249bfcef052aff91e7717a8f
RickySolon-UCM <Solonr491@gmail.com>
2025-10-22 09:47:52 +0800

    Add DebugSix1.java to calculate money growth

---
+++ Chapter_6/DebugSix1.java
@@ -0,0 +1,23 @@
+// Start with a penny
+// Double it every day
+// How much do you have in x number of days?
+
+import java.util.Scanner;
+
+public class DebugSix1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int days;
+        double money = 0.01;
+        int day = 1;
+
+        System.out.print("Enter number of days >> ");
+        days = keyboard.nextInt();
+
+        while (day <= days) {
+            System.out.println("After day " + day + " you have $" + String.format("%.2f", money));
+            money = 2 * money;
+            ++day;
+        }
+    }
+}
